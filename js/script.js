03b8ac26a11353d6ddc7e69091c63fbd0d9da441
Slavik Vasyliv <jerry_lviv@yahoo.com>
2016-02-19 01:03:05 +0200

    reduced to one function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 'use strict';
 
-function myFunc() {
-  console.log('Hi');
-  repeat();
+//function myFunc() {
+//  console.log('Hi');
+//  repeat();
+//}
+//function repeat(){
+//	setTimeout(myFunc, 1000);	
+//}
+//myFunc();
+
+
+
+function makeMyOwnInterval() {
+    console.log('Hi!');
+    setTimeout(makeMyOwnInterval, 1000);
 }
-function repeat(){
-	setTimeout(myFunc, 1000);	
-}
-myFunc();
+makeMyOwnInterval();
